6e25b52e539e2206e55dcad625883cf7f8eb4cee
Khusika Dhamar Gusti <mail@khusika.com>
2020-11-16 22:42:42 +0700

    clang: Update to clang-r399163b
    Taken from:
    https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+/225d2925136a2268ac95a97db4e90d33043b18b9/clang-r399163b/

--- include/llvm/Config/llvm-config.h
+++ include/llvm/Config/llvm-config.h
@@ -69,10 +69,10 @@
 #define LLVM_VERSION_MINOR 0
 
 /* Patch version of the LLVM API */
-#define LLVM_VERSION_PATCH 2
+#define LLVM_VERSION_PATCH 5
 
 /* LLVM version string */
-#define LLVM_VERSION_STRING "11.0.2git"
+#define LLVM_VERSION_STRING "11.0.5git"
 
 /* Whether LLVM records statistics for use with GetStatistics(),
  * PrintStatistics() or PrintStatisticsJSON()
